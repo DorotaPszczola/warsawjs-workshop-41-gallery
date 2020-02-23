1f7f01437a9ca4b141fd4b1cb706637b834c4794
Dorota Pszczola <d.a.pszczola@gmail.com>
2020-02-23 12:45:08 +0100

    refactoring js code

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,26 @@
+function renderPhoto(photo) {
+    console.log('photo', photo);
+
+    // create HTMLImageElement
+    const $photo = document.createElement('img');
+    $photo.src = photo;
+    // równoznaczne z linijka wyżej:  $photo.setAttribute('src', photo)
+
+    // Render
+    const $photos = document.querySelector("#photos");
+    $photos.append($photo);
+}
+
+function renderPhotos(photos) {
+    photos.forEach(function (photo) {
+        renderPhoto(photo);
+
+    });
+}
+
+
+
+
 function main() {
     console.log("main function file works ");
     const photos = [
@@ -5,20 +28,7 @@ function main() {
         "https://i.picsum.photos/id/1035/200/300.jpg",
         "https://i.picsum.photos/id/1033/200/300.jpg"
     ];
-    // console.log(photos);
-    for (let i=0; i<photos.length; i++) {
-        const photo = photos[i];
-        console.log('photo', i, photo);
-
-        // create HTMLImageElement
-        const $photo = document.createElement('img');
-        $photo.src = photo;
-        // równoznaczne z linijka wyżej:  $photo.setAttribute('src', photo)
-
-        // Render
-        const $photos = document.querySelector("#photos");
-        $photos.append($photo);
-    }
+    renderPhotos(photos);  // <- jako argument wchodzi kolekcja
 };
 main();
 
