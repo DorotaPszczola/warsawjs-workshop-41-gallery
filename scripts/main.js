ec5c6ff3da0e8d70564282d4428032c54794489e
Dorota Pszczola <d.a.pszczola@gmail.com>
2020-02-23 13:07:49 +0100

    creating display message for no photos

--- scripts/main.js
+++ scripts/main.js
@@ -19,7 +19,38 @@ function renderPhotos(photos) {
 }
 
 
+function displayMessage() {
+    const $photos = document.querySelector('#photos');
+    const $message = document.createElement('div');
 
+    $message.classList.add('message');  // adding class
+    //  $message.id = 'message';   // adding id
+
+    const $header = document.createElement('div');
+    $header.classList.add('message-header');
+    $header.textContent = "Message to the user";
+
+    const $body = document.createElement('div');
+    $body.classList.add('message-body');
+    $body.textContent = 'Gallery is empty. There are no photos.';
+    
+    $message.append($header);
+    $message.append($body);
+    $photos.append($message);
+    // $message.textContent = "Gallery is empty";
+
+    // <div class="message">
+    //     <div class="message-header">
+    //         message header
+
+    //      </div>
+    //      <div class="message-body">
+    //         message body
+    //      </div>
+    //  </div>
+
+
+}
 
 function main() {
     console.log("main function file works ");
@@ -28,7 +59,8 @@ function main() {
         "https://i.picsum.photos/id/1035/200/300.jpg",
         "https://i.picsum.photos/id/1033/200/300.jpg"
     ];
-    renderPhotos(photos);  // <- jako argument wchodzi kolekcja
+    // renderPhotos(photos);  // <- jako argument wchodzi kolekcja
+    displayMessage();
 };
 main();
 
