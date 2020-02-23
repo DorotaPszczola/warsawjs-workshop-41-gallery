0876b27777457f2c9c66a75c72345c34890f93bb
Dorota Pszczola <d.a.pszczola@gmail.com>
2020-02-23 14:14:11 +0100

    check if there are any photos

--- scripts/main.js
+++ scripts/main.js
@@ -19,36 +19,40 @@ function renderPhotos(photos) {
 }
 
 
-function displayMessage() {
+function displayMessage(message) {
     const $photos = document.querySelector('#photos');
     const $message = document.createElement('div');
 
     $message.classList.add('message');  // adding class
     //  $message.id = 'message';   // adding id
 
-    const $header = document.createElement('div');
-    $header.classList.add('message-header');
-    $header.textContent = "Message to the user";
+    // const $header = document.createElement('div');
+    // $header.classList.add('message-header');
+    // $header.textContent = "Message to user";  // textContent jest szybsze od innerHTML i dużo szybsze od innerText
 
-    const $body = document.createElement('div');
-    $body.classList.add('message-body');
-    $body.textContent = 'Gallery is empty. There are no photos.';
+    // const $body = document.createElement('div');
+    // $body.classList.add('message-body');
+    // $body.textContent = 'Gallery is empty. There are no photos.';
     
-    $message.append($header);
-    $message.append($body);
+    // $message.append($header);
+    // $message.append($body);
     $photos.append($message);
     // $message.textContent = "Gallery is empty";
 
-    // <div class="message">
-    //     <div class="message-header">
-    //         message header
+    $message.innerHTML = `
+    <div class="message">
+        <div class="message-header">
+            Message to user
+         </div>
+         <div class="message-body">
+            ${message}
+         </div>
+     </div>
+`;
+}
 
-    //      </div>
-    //      <div class="message-body">
-    //         message body
-    //      </div>
-    //  </div>
-
+function displayEmptyGalleryMessage() {
+    displayMessage("Gallery is empty. There are no photos.");
 
 }
 
@@ -59,8 +63,14 @@ function main() {
         "https://i.picsum.photos/id/1035/200/300.jpg",
         "https://i.picsum.photos/id/1033/200/300.jpg"
     ];
-    // renderPhotos(photos);  // <- jako argument wchodzi kolekcja
-    displayMessage();
+
+    const isGalleryEmpty = (photos.length === 0)
+
+    if (isGalleryEmpty) {
+        renderPhotos(photos);  // <- jako argument wchodzi kolekcja
+    } else {
+        displayEmptyGalleryMessage();  // <- tylko wewnątrz tej funkcji zmieniamy tekst wiadomości (parametr funkcji displayMessage() i możemy ją wywołać wiele razy z tym samym tekstem )
+    }
 };
 main();
 
